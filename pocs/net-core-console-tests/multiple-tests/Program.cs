4fb6315e91aca32bd6bef88ba8160b68d2f0dfe9
Carlos Cortez <carlos.cfcortez@gmail.com>
2022-12-16 22:48:51 -0400

    wip

---
+++ pocs/net-core-console-tests/multiple-tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        #region Testing tasks
+        Task mytaskTest = new Task(() =>
+        {
+            Console.WriteLine("My task console test");
+        });
+        mytaskTest.Start();
+        mytaskTest.Wait();
+        Console.WriteLine("After start");
+
+        #endregion
+    }
+}
